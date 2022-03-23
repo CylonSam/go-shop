23f6579dbe6e0ab078a3168428898b38e93aebb8
CylonSam <samanthaathayde@hotmail.com>
2022-03-23 11:34:31 -0300

    Add connection to database

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 type Product struct {
+	Id          int
 	Name        string
 	Description string
 	Price       float64
 	Quantity    int
+}
+
+func connectToDB() *sql.DB {
+	connection := "user=sam dbname=goshop password=test123 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
@@ -20,10 +35,35 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	products := []Product{
-		{Name: "T-Shirt", Description: "Blue and beautiful", Price: 50, Quantity: 3},
-		{"Sneakers", "Comfortable and stylish", 299, 5},
-		{"Headphones", "Good!", 49, 10},
+	db := connectToDB()
+
+	allProducts, err := db.Query("select * from products")
+
+	if err != nil {
+		panic(err)
 	}
+
+	p := Product{}
+	products := []Product{}
+
+	for allProducts.Next() {
+		var id, quantity int
+		var name, description string
+		var price float64
+
+		err = allProducts.Scan(&id, &name, &description, &price, &quantity)
+
+		if err != nil {
+			panic(err)
+		}
+
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Quantity = quantity
+
+		products = append(products, p)
+	}
+
 	templates.ExecuteTemplate(w, "Index", products)
 }
